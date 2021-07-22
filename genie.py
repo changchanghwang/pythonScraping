3eda371dc47061a58bebf6ae7e686eced5410f9d
CH_Hwang <hch950627@naver.com>
2021-07-22 23:55:18 +0900

    genie rank scraping

---
+++ genie.py
@@ -0,0 +1,22 @@
+import requests
+from bs4 import BeautifulSoup
+
+from pymongo import MongoClient
+client = MongoClient('localhost', 27017)
+db = client.dbsparta
+
+headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get('https://www.genie.co.kr/chart/top200?ditc=D&ymd=20200403&hh=23&rtm=N&pg=1',headers=headers)
+
+soup = BeautifulSoup(data.text, 'html.parser')
+#body-content > div.newest-list > div > table > tbody > tr:nth-child(2) > td.number
+trs = soup.select('#body-content > div.newest-list > div > table > tbody > tr')
+for tr in trs:
+    ranks = tr.select_one('td.number')
+    titles = tr.select_one('td.info > a.title.ellipsis')
+    artists = tr.select_one('td.info > a.artist.ellipsis')
+    rank = ranks.text[0:2].strip()
+    title = titles.text.strip()
+    artist = artists.text.strip()
+
+    print(rank, 'ㅣ', title, 'ㅣ', artist)
